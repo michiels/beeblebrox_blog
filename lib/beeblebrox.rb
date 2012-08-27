6486b4332131cb35f15008bdd895415a283dfe78
Michiel Sikkes <michiel@firmhouse.com>
2012-08-27 15:38:33 +0200

    Allow setting of api endpoint

--- lib/beeblebrox.rb
+++ lib/beeblebrox.rb
@@ -2,4 +2,10 @@ require 'beeblebrox/engine'
 
 module Beeblebrox
   mattr_accessor :site_id
+  mattr_accessor :api_endpoint
+  
+  def self.api_endpoint
+    @@api_endpoint ||= "http://www.beeblebroxapp.com"
+    return @@api_endpoint
+  end
 end
