80ef7d14f1db03e49f28e3dbf49fd83c0a06e415
Michiel Sikkes <michiel@firmhouse.com>
2012-08-27 15:44:10 +0200

    Set correct default endpoint

--- app/controllers/beeblebrox/blog_controller.rb
+++ app/controllers/beeblebrox/blog_controller.rb
@@ -4,7 +4,7 @@ require 'faraday_middleware'
 module Beeblebrox
   class BlogController < ApplicationController
     def index
-      conn = Faraday.new(:url => 'http://www.beeblebroxapp.com') do |faraday|
+      conn = Faraday.new(:url => Beeblebrox.api_endpoint) do |faraday|
         faraday.response :json, :content_type => /\bjson$/
         faraday.adapter Faraday.default_adapter
       end
@@ -14,7 +14,7 @@ module Beeblebrox
     end
     
     def show
-      conn = Faraday.new(:url => 'http://www.beeblebroxapp.com') do |faraday|
+      conn = Faraday.new(:url => Beeblebrox.api_endpoint) do |faraday|
         faraday.response :json, :content_type => /\bjson$/
         faraday.adapter Faraday.default_adapter
       end
