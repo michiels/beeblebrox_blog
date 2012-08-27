e8e1dd6ae1572bbe49b80d2b28059b06dd9819cc
Michiel Sikkes <michiel@firmhouse.com>
2012-08-27 15:35:25 +0200

    Added single post view

--- app/controllers/beeblebrox/blog_controller.rb
+++ app/controllers/beeblebrox/blog_controller.rb
@@ -4,13 +4,23 @@ require 'faraday_middleware'
 module Beeblebrox
   class BlogController < ApplicationController
     def index
-      conn = Faraday.new(:url => 'http://www.beeblebroxapp.com') do |faraday|
+      conn = Faraday.new(:url => 'http://www.beeblebroxapp.dev') do |faraday|
         faraday.response :json, :content_type => /\bjson$/
         faraday.adapter Faraday.default_adapter
       end
     
       response = conn.get "/#{Beeblebrox.site_id}/api/posts.json"
       @posts = response.body
+    end
+    
+    def show
+      conn = Faraday.new(:url => 'http://www.beeblebroxapp.dev') do |faraday|
+        faraday.response :json, :content_type => /\bjson$/
+        faraday.adapter Faraday.default_adapter
+      end
+      
+      response = conn.get "/#{Beeblebrox.site_id}/api/posts/#{params[:id]}.json"
+      @post = response.body
     end
   end
 end
