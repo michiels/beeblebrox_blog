2bc5d8e9fa48a2ad534d7e0451dba14f46938104
Michiel Sikkes <michiel@firmhouse.com>
2012-08-27 12:35:22 +0200

    Allow configuring of site id

--- app/controllers/beeblebrox/blog_controller.rb
+++ app/controllers/beeblebrox/blog_controller.rb
@@ -4,12 +4,12 @@ require 'faraday_middleware'
 module Beeblebrox
   class BlogController < ApplicationController
     def index
-      conn = Faraday.new(:url => 'http://www.beeblebrox.dev') do |faraday|
+      conn = Faraday.new(:url => 'http://www.beeblebroxapp.com') do |faraday|
         faraday.response :json, :content_type => /\bjson$/
         faraday.adapter Faraday.default_adapter
       end
     
-      response = conn.get "/811047914/api/posts.json"
+      response = conn.get "/#{Beeblebrox.site_id}/api/posts.json"
       @posts = response.body
     end
   end
--- lib/beeblebrox.rb
+++ lib/beeblebrox.rb
@@ -1,4 +1,5 @@
 require 'beeblebrox/engine'
 
 module Beeblebrox
+  mattr_accessor :site_id
 end
